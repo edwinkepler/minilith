2cfcaff13156ce4e0975de5ad1e60802f779a653
edwinkepler <edwinkepler@protonmail.com>
2018-06-13 23:06:58 +0200

    Add css CDN links and app database query info

--- themes/material/header.php
+++ themes/material/header.php
@@ -2,17 +2,21 @@
 
 ?>
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="<?php echo $_APP["language"] ?>">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <meta name="description" content="">
+    <title><?php echo $_APP["title"] ?></title>
+    <meta name="description" content="<?php echo $_APP["description"] ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="https://yarn.pm/normalize.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <link rel="stylesheet" href="css/main.css">
+
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
 
 <body>
