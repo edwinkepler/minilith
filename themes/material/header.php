329df3d111d5846a403ffc6ad286ac7d363825be
edwinkepler <edwinkepler@protonmail.com>
2018-06-16 15:17:14 +0200

    Update material theme header.php

--- themes/material/header.php
+++ themes/material/header.php
@@ -2,21 +2,49 @@
 
 ?>
 <!doctype html>
-<html class="no-js" lang="<?php echo $_APP["language"] ?>">
+<html class="no-js" lang="<?php echo $main->app()->getTitle(); ?>">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php echo $_APP["title"] ?></title>
-    <meta name="description" content="<?php echo $_APP["description"] ?>">
+    <title><?php echo $main->app()->getTitle(); ?></title>
+    <meta name="description" content="<?php echo $main->app()->getDescryption(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://yarn.pm/normalize.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="<?php echo $main->theme()->getThemeUrl(); ?>/css/main.css">
 
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
 
 <body>
+    <div class="layout-transparent mdl-layout mdl-js-layout">
+        <header class="mdl-layout__header mdl-layout__header--transparent">
+            <div class="mdl-layout__header-row">
+                <!-- Title -->
+                <span class="mdl-layout-title"><?php echo $main->app()->getTitle(); ?></span>
+                <!-- Add spacer, to align navigation to the right -->
+                <div class="mdl-layout-spacer"></div>
+                <!-- Navigation -->
+                <nav class="mdl-navigation">
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                </nav>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title"><?php echo $main->app()->getTitle(); ?></span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+            </nav>
+        </div>
+        <main class="mdl-layout__content">
+        </main>
+    </div>
