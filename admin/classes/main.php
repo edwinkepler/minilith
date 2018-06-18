d063996539db4684d0b2ce3effcdde6228150dc0
edwinkepler <edwinkepler@protonmail.com>
2018-06-18 18:04:37 +0200

    Add Main class

---
+++ admin/classes/main.php
@@ -0,0 +1,23 @@
+<?php
+    require("../config/app.php");
+    require("../config/db.php");
+    require("session.php");
+    require("db.php");
+
+    class Main {
+        private $db;
+        private $config_app;
+
+        public function __construct($_db, $_app) {
+            $this->db = new DB($_db, $_app);
+        }
+
+        public function db() {
+            return $this->db;
+        }
+
+        public static function setPageName($_name) {
+            define("PAGE_NAME", $_name);
+        }
+    }
+?>
