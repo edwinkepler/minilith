b2ced732f65f9956495311e51965b3045294b3bf
edwinkepler <edwinkepler@protonmail.com>
2018-06-28 20:53:46 +0200

    Fix db config variables names

--- admin/includes/login/login.read.php
+++ admin/includes/login/login.read.php
@@ -1,6 +1,7 @@
 <?php
 ob_start();
 session_start();
+
 if (isset($_POST)) {
     if ($_SESSION["csrf"] != $_POST["csrf"] || !isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["submit"])) {
         header("Location: ../../login.php");
@@ -12,13 +13,14 @@ if (isset($_POST)) {
 }
 
 require("../../../config/db.php");
-$connection = mysqli_connect($db["host"], $db["user"], $db["pass"], $db["name"]);
+$connection = mysqli_connect($config_db["host"], $config_db["user"], $config_db["pass"], $config_db["name"]);
 
 $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
 $email = $connection->real_escape_string($email);
 $password = $connection->real_escape_string($_POST["password"]);
 
 $query = mysqli_query($connection, "SELECT * FROM users WHERE email='$email'");
+
 if (mysqli_num_rows($query) == 0) {
     header("Location: ../../login.php");
     exit;
@@ -34,4 +36,3 @@ if (mysqli_num_rows($query) == 0) {
         exit;
     }
 }
-?>
