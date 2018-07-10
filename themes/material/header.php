a162d1a14d843f6a72e72d855addab1505540574
edwinkepler <edwinkepler@protonmail.com>
2018-07-10 19:49:47 +0200

    Get rid of bulma css framework

--- themes/material/header.php
+++ themes/material/header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="<?php echo $main->site()->description(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $main->theme()->url(); ?>/assets/css/main.css">
 </head>
