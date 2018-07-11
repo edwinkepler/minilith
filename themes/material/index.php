16a16db8624361000b852bdce10c03bd7ff58340
edwinkepler <edwinkepler@protonmail.com>
2018-07-11 21:46:56 +0200

    Update material theme index

--- themes/material/index.php
+++ themes/material/index.php
@@ -3,21 +3,28 @@
 <div class="background"></div>
 <main class="container">
     <div class="columns">
-        <div class="column">
+        <div class="column margin-top-15rem">
             <?php
-            if($main->posts()->count() == 0) {
+            if ($main->posts()->count() == 0) {
                 echo "<h1>No posts found</h1>";
             } else {
-                foreach($main->posts()->all() as $post) {
+                foreach ($main->posts()->all() as $post) {
             ?>
             <a href="index.php?post=<?php echo $post["id"] ?>">
                 <div class="card">
-                    <div class="card-img" style="background-image: url('<?php echo $main->storage()->images() . $post["image"] ?>')">
+                    <div class="card-img" style="background-image: url('<?php echo $main->storage()->imagesUrl() . $post["image"] ?>')">
                         <div class="card-title-shader"></div>
                         <h5 class="card-title"><?php echo $post["title"] ?></h5>
                     </div>
                     <div class="card-body">
                         <p class="card-text"><?php echo $post["excerpt"] ?></p>
+                    </div>
+                    <div class="meta">
+                        <div class="avatar"><img src="<?php echo $main->storage()->avatarsUrl() . $main->users()->avatar($post['author']); ?>" alt="<?php echo $post['author'] ?>"></div>
+                        <div class="author-date">
+                            <div class="author"><?php echo $post['author']; ?></div>
+                            <div class="date"><?php echo $post['date_published']; ?></div>
+                        </div>
                     </div>
                 </div>
             </a>
@@ -28,6 +35,5 @@
         </div>
     </div>
 </main>
-
 
 <?php include "footer.php"; ?>
