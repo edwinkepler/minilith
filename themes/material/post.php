09dbc2f9fb8f7cba5d1e165f56ff38993fc23b87
edwinkepler <edwinkepler@protonmail.com>
2018-07-12 20:19:46 +0200

    Fix disqus

--- themes/material/post.php
+++ themes/material/post.php
@@ -28,7 +28,7 @@
                     <div id="disqus_thread"></div>
                     <script>
                         var disqus_config = function () {
-                            this.page.url = $main->appUrl();  // Replace PAGE_URL with your page's canonical URL variable
+                            this.page.url = "<?php echo $main->appUrl(); ?>";  // Replace PAGE_URL with your page's canonical URL variable
                             this.page.identifier = <?php echo $main->post()->id(); ?>; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
                         };
                         (function() { // DON'T EDIT BELOW THIS LINE
