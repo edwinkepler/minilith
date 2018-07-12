efd059e44f418070289a47ad1d5cc926ac45eff4
edwinkepler <edwinkepler@protonmail.com>
2018-07-12 20:03:27 +0200

    Add appUrl() method

--- classes/class.main.php
+++ classes/class.main.php
@@ -68,4 +68,9 @@ class Main
     {
         return $this->users;
     }
+
+    public function appUrl() : string
+    {
+        return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    }
 }
--- themes/material/post.php
+++ themes/material/post.php
@@ -27,20 +27,15 @@
                 <div class="card-comments">
                     <div id="disqus_thread"></div>
                     <script>
-                        /**
-                        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-                        /*
                         var disqus_config = function () {
-                        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-                        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                            this.page.url = $main->appUrl();  // Replace PAGE_URL with your page's canonical URL variable
+                            this.page.identifier = <?php echo $main->post()->id(); ?>; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
                         };
-                        */
                         (function() { // DON'T EDIT BELOW THIS LINE
-                        var d = document, s = d.createElement('script');
-                        s.src = 'https://edwinkepler.disqus.com/embed.js';
-                        s.setAttribute('data-timestamp', +new Date());
-                        (d.head || d.body).appendChild(s);
+                            var d = document, s = d.createElement('script');
+                            s.src = 'https://edwinkepler.disqus.com/embed.js'; // Change this line!
+                            s.setAttribute('data-timestamp', +new Date());
+                            (d.head || d.body).appendChild(s);
                         })();
                     </script>
                 </div>
