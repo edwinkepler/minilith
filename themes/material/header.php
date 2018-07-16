76e1e5d0973523bdc67ab5def2455c0a34d49bc2
edwinkepler <edwinkepler@protonmail.com>
2018-07-16 20:30:53 +0200

    Add open graph tags to posts pages

--- themes/material/header.php
+++ themes/material/header.php
@@ -8,6 +8,20 @@
     <meta name="description" content="<?php echo $main->site()->description(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <?php
+    if ($main->isPost()) {
+    ?>
+    <meta property="og:title" content="<?php echo $main->post()->title(); ?>" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="<?php echo $main->appUrl(); ?>" />
+    <meta property="og:image" content="<?php echo $main->storage()->imagesUrl() . $main->post()->image(); ?>" />
+    <meta property="og:description" content="<?php echo strip_tags(implode(' ', array_slice(explode(' ', $main->post()->content()), 0, 20))) . '...'; ?>" />
+    <meta property="og:locale" content="<?php echo $main->site()->language(); ?>" />
+    <meta property="og:site_name" content="<?php echo $main->site()->title(); ?>" />
+    <?php
+    }
+    ?>
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <?php
